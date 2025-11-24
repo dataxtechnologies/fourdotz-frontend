84c07de2b6db4144d619a97ea712a571204c1ecd
Naveen <fullstack@wowelse.com>
2025-11-24 14:59:28 +0530

    invoice changes

--- src/app/services/auth/urlhelpers.service.ts
+++ src/app/services/auth/urlhelpers.service.ts
@@ -7,8 +7,8 @@ import { Injectable } from '@angular/core';
 export class UrlhelpersService {
   public getAPIURL(): string {
     // return 'http://127.0.0.1:5000/api/v1';
-    // return "https://api.fourdotz.com/api/v1" // production
-    return "https://dev-api.fourdotz.com/api/v1" // development
+    return "https://api.fourdotz.com/api/v1" // production
+    // return "https://dev-api.fourdotz.com/api/v1" // development
     // return "https://fourdotz-backend.onrender.com/api/v1" //render development
   }
 }
